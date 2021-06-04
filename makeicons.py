adb4829c9ca1688fd5abe345560fe557628c3653
atgiphy3750 <plastic041@gmail.com>
2021-06-04 04:15:23 +0000

    makeicons.py: uses byte buffer without saving

--- makeicons.py
+++ makeicons.py
@@ -1,36 +1,38 @@
 from cairosvg import svg2png
 from PIL import Image
 import sys
-
+import io
 
 def simpleicons2png(name: str, size: int = 512):
     print(f"getting {name} from simpleicons...")
     url = f"https://simpleicons.org/icons/{name}.svg"
     print(f"{name} recieved.")
     try:
-        svg2png(url=url,
-                write_to=f'./icons/{name}.png',
+        buffer = svg2png(url=url,
                 output_height=size,
                 output_width=size)
-        print(f"{name} converted and saved.")
+        print(f"{name} converted to buffer")
+        return buffer
     except Exception as e:
         print(f"cannot convert {name}. error: {e}")
+        return False
 
 
-def remove_alpha(name: str):
-    image = Image.open(f"./icons/{name}.png").convert("RGBA")
+def remove_alpha(buffer, name: str):
+    image = Image.open(io.BytesIO(buffer)).convert("RGBA")
     bg = Image.new("RGBA", image.size, (255, 255, 255))
     result = Image.alpha_composite(bg, image)
     result.save(f"./icons/{name}.png", "PNG")
-    print(f"removed alpha and saved {name}.png")
+    print(f"removed alpha and saved {name}.png.")
 
 
 def main():
     if len(sys.argv) > 1:
         svgnames = sys.argv[1:]
         for svgname in svgnames:
-            simpleicons2png(svgname)
-            remove_alpha(svgname)
+            buffer = simpleicons2png(svgname)
+            if buffer:
+                remove_alpha(buffer, svgname)
 
     else:
         print("no name given. abort.")
