71749601f8d50f0abf07d586d051f0a2457b088f
atgiphy3750 <plastic041@gmail.com>
2021-06-04 16:12:31 +0900

    makeicons: rainmater done, need refactoring

--- makeicons.py
+++ makeicons.py
@@ -1,18 +1,19 @@
 from cairosvg import svg2png
 from PIL import Image, ImageOps
-import numpy as np
 import sys
 import io
+import pathlib
+
+SIZE = 128
+RATIO = 0.6
 
 
-def simpleicons2image(name: str, size: int = 512):
+def simpleicons2image(name: str, size: int = SIZE):
     print(f"getting {name} from simpleicons...")
     url = f"https://simpleicons.org/icons/{name}.svg"
     print(f"{name} recieved.")
     try:
-        buffer = svg2png(url=url,
-                         output_height=size,
-                         output_width=size)
+        buffer = svg2png(url=url, output_height=size, output_width=size)
         image = Image.open(io.BytesIO(buffer)).convert("RGBA")
         print(f"{name} converted to Image")
         return image
@@ -21,55 +22,59 @@ def simpleicons2image(name: str, size: int = 512):
         return False
 
 
-def image_color(image, color):
-    data = np.array(image)
-    r, g, b, a = data.T
-    area = (r == 255) & (g == 255) & (b == 255)
-    data[..., :-1][area.T] = color
-    result = Image.fromarray(data)
+def image_color(shape, color_front, color_back):
+    mask = shape.convert("L")
+    front = Image.new("RGBA", shape.size, color_front)
+    back = Image.new("RGBA", shape.size, color_back)
+    result = Image.composite(front, back, mask)
+
     return result
 
 
-def image_invert(image) -> Image:
-    if image.mode == 'RGBA':
+def image_bg(image, color):
+    bg = Image.new("RGBA", (SIZE, SIZE), color)
+    side = int(SIZE * RATIO)
+    pos = SIZE // 2 - side // 2
+    bg.paste(image, (pos, pos), image)
+
+    return bg
+
+
+def image_invert(image):
+    if image.mode == "RGBA":
         r, g, b, a = image.split()
-        rgb_image = Image.merge('RGB', (r, g, b))
-
+        rgb_image = Image.merge("RGB", (r, g, b))
         inverted_image = ImageOps.invert(rgb_image)
-
         r2, g2, b2 = inverted_image.split()
-
-        inverted = Image.merge('RGBA', (r2, g2, b2, a))
+        result = Image.merge("RGBA", (r2, g2, b2, a))
     else:
-        inverted = ImageOps.invert(image)
-    return inverted
-
-
-def image_bg(image, name: str, color):
-    bg = Image.new("RGBA", image.size, color)
-    result = Image.alpha_composite(bg, image)
+        result = ImageOps.invert(image)
     return result
 
 
 def rainmeterpng(name, color):
-    image = simpleicons2image(name)
-    if image:
-        image_main_shape = image_color(image, color)
-        image_main = image_bg(image_main_shape, name, "#ffffff00")
+    shape = simpleicons2image(name)
+    if shape:
+        shape_resized = ImageOps.scale(shape, RATIO, Image.LANCZOS)
 
-        image_over_shape = image_color(image, "#ffffff00")
-        image_over = image_bg(image_over_shape, name, color)
+        mask_uninverted = image_bg(shape_resized, "#ffffffff")
+        mask = image_invert(mask_uninverted)
 
-        image_main.save(f"./icons/{name}.png", "PNG")
-        image_over.save(f"./icons/{name}_over.png", "PNG")
-        print(f"Saved {name}.png with given background color.")
+        image_main = image_color(mask, "#ffffffff", color)
+        image_over = image_color(mask, color, "#ffffffff")
+
+        pathlib.Path(f"./icons/{name}").mkdir(exist_ok=True)
+
+        image_main.save(f"./icons/{name}/{name}.png", "PNG")
+        image_over.save(f"./icons/{name}/{name}_over.png", "PNG")
+        print(f"Saved {name}.png and {name}_over.png with given background color.")
 
 
 def simpleicons2png(names):
     for name in names:
         buffer = simpleicons2image(name)
         if buffer:
-            image_bg(buffer, name, "#ffffffff")
+            image_bg(buffer, "#ffffffff")
 
 
 def main():
@@ -82,9 +87,11 @@ def main():
         elif type == "rainmeter":
             rainmeterpng(names[0], names[1])
         elif type == "-h" or type == "--help":
-            print("""
+            print(
+                """
             firefox, discord
-            """)
+            """
+            )
     else:
         print("no name given. abort.")
 
